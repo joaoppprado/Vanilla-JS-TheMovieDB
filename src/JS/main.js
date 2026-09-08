851ba6bbe0e235e8d2469fc5acdc49d8a343a83f
João Pedro P. Prado <joaoprado2789@gmail.com>
2026-09-08 16:28:11 -0300

    import API e UI, cria funcoes para pegar filmes populares e bem avaliados da API para pagina home

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,4 +1,6 @@
 //O "ponto de entrada" que liga os eventos da tela às funções
+import { API } from "./api.js";
+import { UI } from "./ui.js";
 
 const menuButton = document.querySelector(".navigation__toggle");
 const menu = document.querySelector(".navigation__menu");
@@ -14,3 +16,32 @@ if (menuButton && menu) {
     menu.classList.toggle("hidden", isOpen);
   });
 }
+
+async function homePopularMovies() {
+  const container = document.querySelector(".section__movie-cards-popular");
+  try {
+    const data = await API.getPopularMovies();
+    const homeMovies = data.results.slice(0, 10);
+
+    UI.renderMovieList(homeMovies, container);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function homeTopMovies() {
+  const container = document.querySelector(
+    ".section__movie-cards-new-releases",
+  );
+  try {
+    const data = await API.getTopRatedMovies();
+    const homeMovies = data.results.slice(0, 10);
+
+    UI.renderMovieList(homeMovies, container);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+homePopularMovies();
+homeTopMovies();
