e14a4d83524a1a0355310f388f48b513cbb9439b
João Pedro P. Prado <joaoprado2789@gmail.com>
2026-09-08 16:24:15 -0300

    modifica funcao new releases para top rated, export para funcoes prontas

--- src/JS/api.js
+++ src/JS/api.js
@@ -28,8 +28,8 @@ async function getPopularMovies(page = 1) {
   }
 }
 
-async function getReleaseMovies(page = 1) {
-  const url = `${BASE_URL}/movie/now_playing?language=en-US&page=${page}`;
+async function getTopRatedMovies(page = 1) {
+  const url = `${BASE_URL}/movie/top_rated?language=en-US&page=${page}`;
   try {
     const response = await fetch(url, {
       method: "GET",
@@ -41,14 +41,14 @@ async function getReleaseMovies(page = 1) {
 
     if (!response.ok) {
       throw new Error(
-        `Error: Get now playing movies didn't work. API code error = ${response.status}`,
+        `Error: Get top rated movies didn't work. API code error = ${response.status}`,
       );
     }
 
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error("API error (getReleaseMovies): ", error);
+    console.error("API error (getTopRatedMovies): ", error);
     throw error;
   }
 }
@@ -81,3 +81,9 @@ async function getFilteredMovies(genreId, sortBy) {
 async function getMovieDetails(id) {}
 
 async function searchMovieByName(query) {}
+
+export const API = {
+  getPopularMovies,
+  getTopRatedMovies,
+  getFilteredMovies,
+};
