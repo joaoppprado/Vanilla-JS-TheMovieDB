aa7276d162dfdcac8dd26969a9eb294dc4bbba8a
João Pedro P. Prado <joaoprado2789@gmail.com>
2026-09-09 15:25:32 -0300

    adiciona get filmes popular e top rated  baseado na página atual

--- src/JS/main.js
+++ src/JS/main.js
@@ -4,6 +4,9 @@ import { UI } from "./ui.js";
 
 const menuButton = document.querySelector(".navigation__toggle");
 const menu = document.querySelector(".navigation__menu");
+const isHomePage = document.getElementById("home-page");
+const isPopularPage = document.getElementById("popular-page");
+const isTopRatedPage = document.getElementById("top-rated-page");
 
 if (menuButton && menu) {
   menuButton.addEventListener("click", () => {
@@ -18,7 +21,7 @@ if (menuButton && menu) {
 }
 
 async function homePopularMovies() {
-  const container = document.querySelector(".section__movie-cards-popular");
+  const container = document.querySelector("#home-popular");
   try {
     const data = await API.getPopularMovies();
     const homeMovies = data.results.slice(0, 10);
@@ -30,7 +33,7 @@ async function homePopularMovies() {
 }
 
 async function homeTopMovies() {
-  const container = document.querySelector(".section__movie-cards-top-rated");
+  const container = document.querySelector("#home-top-rated");
   try {
     const data = await API.getTopRatedMovies();
     const homeMovies = data.results.slice(0, 10);
@@ -41,5 +44,45 @@ async function homeTopMovies() {
   }
 }
 
-homePopularMovies();
-homeTopMovies();
+async function PagePopularMovies() {
+  const container = document.querySelector("#popular-movies");
+  try {
+    const page1 = await API.getPopularMovies();
+    const page2 = await API.getPopularMovies(2);
+    const page3 = await API.getPopularMovies(3);
+
+    const movies = [...page1.results, ...page2.results, ...page3.results];
+
+    UI.renderMovieList(movies, container);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function PageTopRatedMovies() {
+  const container = document.querySelector("#top-rated-movies");
+  try {
+    const page1 = await API.getTopRatedMovies();
+    const page2 = await API.getTopRatedMovies(2);
+    const page3 = await API.getTopRatedMovies(3);
+
+    const movies = [...page1.results, ...page2.results, ...page3.results];
+
+    UI.renderMovieList(movies, container);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (isHomePage) {
+    homePopularMovies();
+    homeTopMovies();
+  }
+  if (isPopularPage) {
+    PagePopularMovies();
+  }
+  if (isTopRatedPage) {
+    PageTopRatedMovies();
+  }
+});
